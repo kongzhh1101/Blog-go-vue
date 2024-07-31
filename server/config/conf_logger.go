402e0e67d13c21d60ff578d9ead332447941bf1f
kongzhh <kongzhh1101@gmail.com>
2024-07-31 16:17:19 +0800

    commit2

--- server/config/conf_logger.go
+++ server/config/conf_logger.go
@@ -3,7 +3,7 @@ package config
 type Logger struct {
 	Level       string `yaml:"level"`
 	Perfix      string `yaml:"perfix"`
-	Director    bool   `yaml:"director"`
+	Director    string `yaml:"director"`
 	ShowLine    bool   `yaml:"show_line"` //是否显示行号
 	LogInConsol bool   `yaml:"log_in_consol"`
 }
