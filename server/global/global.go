3401773fd80347912ea1f50c0017abfc8c010a4a
kongzhh <kongzhh1101@gmail.com>
2024-07-31 21:17:53 +0800

    日志配置

--- server/global/global.go
+++ server/global/global.go
@@ -3,10 +3,12 @@ package global
 import (
 	"Blog/config"
 
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 var (
 	Config *config.Config
 	DB     *gorm.DB
+	Log    *logrus.Logger
 )
--- server/main.go
+++ server/main.go
@@ -7,7 +7,14 @@ import (
 )
 
 func main() {
+	// 读取配置文件
 	core.InitConf()
+	// 初始化日志
+	global.Log = core.InitLogger()
+	global.Log.Warnln("aaa")
+	global.Log.Error("aaa")
+	global.Log.Info("aaa")
+	// 连接数据库
 	global.DB = core.InitGorm()
 	fmt.Println(global.DB)
 }
